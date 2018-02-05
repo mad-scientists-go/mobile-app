25bc37e1ac71ee60131dcb801d9bf91863456179
ray <raytorres280@gmail.com>
2018-02-05 16:53:12 -0500

    form

--- components/SignUp.js
+++ components/SignUp.js
@@ -5,38 +5,42 @@ import {
   StyleSheet,
   TextInput,
   TouchableHighlight,
-  Modal
+  Modal,
+  Image
 } from "react-native";
 import { Camera, Permissions } from "expo";
-import { Icon, Button } from "react-native-elements";
-import SignUpCamera from './SignUpCamera'
+import { Icon, Button, FormInput } from "react-native-elements";
+import SignUpCamera from "./SignUpCamera";
 
 export default class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: "hello world",
-      showCamera: true,
+      showCamera: false,
       hasCameraPermission: null,
       type: Camera.Constants.Type.front,
       photos: []
     };
-    this.grabPhotos = this.grabPhotos.bind(this)
-    this.toggleCamera = this.toggleCamera.bind(this)
+    this.grabPhotos = this.grabPhotos.bind(this);
+    this.toggleCamera = this.toggleCamera.bind(this);
   }
 
   grabPhotos(photos) {
-    this.setState({ photos })
+    console.log(photos);
+    this.setState({ photos });
   }
 
   toggleCamera() {
-      this.setState({ showCamera: !this.state.showCamera })
+    this.setState({ showCamera: !this.state.showCamera });
   }
 
-  snap() {
+  snap() {}
 
+  handleSignUp() {
+    console.log("signup btn working");
+    //do a post
   }
-
   render() {
     return (
       <View style={styles.container}>
@@ -66,24 +70,40 @@ export default class SignUp extends Component {
           value={this.state.email}
           style={styles.field}
         />
+        <FormInput
+          onChangeText={text => this.setState({ email: text })}
+          value={this.state.email}
+          containerStyle={styles.field}
+        />
+        <FormInput onChangeText={text => this.setState({ email: text })} />
         <View style={styles.footer}>
           <View style={styles.photoContainer}>
-            <Text>pic container</Text>
+            {this.state.photos.map(img => (
+              <Image
+                style={{ height: 50, width: 50, marginLeft: 5, marginRight: 5 }}
+                source={{ uri: img.uri }}
+              />
+            ))}
           </View>
-          <View>
-            <TouchableHighlight onPress={() => console.log("signup")}>
-              <Text>signup</Text>
-            </TouchableHighlight>
-            <TouchableHighlight
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Button
+              color="purple"
+              title="Sign Up"
+              onPress={() => this.handleSignUp()}
+            />
+            <Button
+              color="purple"
+              title="Take Photos"
               onPress={() => this.setState({ showCamera: true })}
-            >
-              <Text>take photos</Text>
-            </TouchableHighlight>
+            />
           </View>
         </View>
 
         <Modal visible={this.state.showCamera}>
-            <SignUpCamera grabPhotos={this.grabPhotos} toggleCamera={this.toggleCamera} />
+          <SignUpCamera
+            grabPhotos={this.grabPhotos}
+            toggleCamera={this.toggleCamera}
+          />
         </Modal>
       </View>
     );
@@ -106,10 +126,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   field: {
-    width: "100%",
+    width: "80%",
     marginTop: 50,
     height: 40,
     color: "red",
     backgroundColor: "red"
+  },
+  photoContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });
